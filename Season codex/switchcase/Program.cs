07d95f7b1ae1ffc5627c0c4569b0637e308cd60f
Eldar Eyvazli <eldareyvazli@gmail.com>
2024-05-23 13:40:41 +0400

    New Code Added or updated
    some changes to the code base : -5.-02-13-40-41

---
+++ Season codex/switchcase/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace switchcase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("enter the season");
+            string season = Console.ReadLine();
+
+            string sea = season switch
+            {
+                "winter" => "Qis",
+
+                "summer" => "yay",
+
+                "spring" => "yaz",
+
+                "authum" => "payiz"
+            };
+        
+            Console.WriteLine(sea);
+
+
+            string season2 = Console.ReadLine();
+
+            switch (season2)
+            {
+                case "winter":
+                    Console.WriteLine("Fesil Qis");
+                    break;
+                case "summer":
+                    Console.WriteLine("Fesil- Yay");
+                    break;
+                case "spring":
+                    Console.WriteLine("Fesil - Yaz");
+                    break;
+                case "autumn":
+                    Console.WriteLine("Fesil -Payiz");
+                    break;
+                default:
+                    Console.WriteLine("Bele fesil yoxdu");
+                    break;
+
+
+            }
+
+
+        }
+    }
+}
