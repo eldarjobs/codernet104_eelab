361a446ee9aa1d7ce46bdbe8d2d69835a564bc65
Eldar E. Eyvazli <eldareey@code.local>
2024-05-27 13:47:38 +0400

    New Code Added or updated
    some changes to the code base : 2024-05-27-13-47-38

---
+++ Lab 27.05.2024/arraycomplexsimplex/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Threading.Channels;
+
+bool IsPrime(int num)
+{
+    if (num <= 1)
+        return false;
+    if (num <= 3)
+        return true;
+
+    if (num % 2 == 0 || num % 3 == 0)
+        return false;
+
+    int i = 5;
+    while (i * i <= num)
+    {
+        if (num % i == 0 || num % (i + 2) == 0)
+            return false;
+        i += 6;
+    }
+
+    return true;
+}
+
+bool[] CheckPrimeNumbers(int[] numbers)
+{
+    bool[] primeFlags = new bool[numbers.Length];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        primeFlags[i] = IsPrime(numbers[i]);
+    }
+    return primeFlags;
+}
+int[] numbers = Console.WriteLine(value: "Eded daxil edin");
+
+bool[] primeFlags = CheckPrimeNumbers(numbers);
---
+++ Lab 27.05.2024/arraymethod/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] input = { 2, 8, 11, -15, 50 };
+
+        void task4(int[] input)
+        {
+            int min = input[0];
+            int minIndex = input[1];
+
+            for (int i = 1; i < input.Length; i++)
+
+            {
+                if (input[i] < min)
+                {
+                    min = input[i];
+                    minIndex = i;
+
+                }
+
+            }
+
+            Console.WriteLine("Minimum value" + min);
+            Console.WriteLine("Minimum value" + minIndex);
+        }
+
+    }
+}
