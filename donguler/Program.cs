6fa27118bf890b070693a27dd4a99357d693c728
Eldar E. Eyvazli <eldareey@code.local>
2024-05-30 13:43:23 +0400

    New Code Added or updated
    some changes to the code base : 2024-05-30-13-43-23

---
+++ donguler/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static string CreateRandomPassword(int passwordLength)
+    {
+      string allowedChars = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789!@$?_-";
+        char[] chars = new char[passwordLength];
+        Random rd = new Random();
+
+        for (int i = 0; i < passwordLength; i++)
+        {
+            chars[i] = allowedChars[rd.Next(0, allowedChars.Length)];
+        }
+
+        return new string(chars);
+    }
+}
