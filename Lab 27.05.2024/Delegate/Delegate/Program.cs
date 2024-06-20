484f93cb1c33ededbd65f61b01a9879ce64f544e
Eldar E. Eyvazli <eldareey@code.local>
2024-06-20 12:02:47 +0400

    New Code Added or updated
    some changes to the code base : 2024-06-20-12-02-46

--- Lab 27.05.2024/Delegate/Delegate/Program.cs
+++ Lab 27.05.2024/Delegate/Delegate/Program.cs
@@ -5,6 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        } 
+        public class Ilogger
+        {
+            public void Log(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+        public class Iloggerlog : Ilogger
+        {
+            public void Log(string message)
+            {
+                Console.WriteLine(message);
+            }
         }
     }
 }
---
+++ Lab 27.05.2024/expressions/Program.cs
@@ -0,0 +1,71 @@
+﻿using expressions.Models;
+using Microsoft.VisualBasic;
+using System.Linq;
+using System.Linq.Expressions;
+#region First Example   
+
+Expression<Func<int, int>> square = x => x * x;
+var result = square.Compile();
+
+Console.WriteLine(result(5));
+
+
+#endregion
+
+#region Second Example
+
+Expression<Func<int, int, bool>> ISGreater = (n1, n2) => n1 > n2;
+
+Func<int, int, bool> IsgreaterFunc = ISGreater.Compile();
+
+Console.WriteLine(IsgreaterFunc(10, 5));
+Console.WriteLine(IsgreaterFunc(5, 10));
+
+#endregion
+
+#region Third Example
+var employees = new List<Employee>
+{
+
+new Employee { FirstName = "John", LastName = "JAck", Age = 25 },
+new Employee { FirstName = "Jane", LastName = "Doe", Age = 30 },
+new Employee { FirstName = "Jack", LastName = "Jony", Age = 35 },
+new Employee { FirstName = "Joe", LastName = "Doe", Age = 45 },
+new Employee { FirstName = "Judy", LastName = "Bakter", Age = 50 },
+new Employee { FirstName = "Jen", LastName = "Brow", Age = 55 },
+};
+
+
+Expression<Func<Employee, bool>> filter = e => e.Age >= 18;
+
+//if (filter.Body is BinaryExpression binary)
+//{
+//    Console.WriteLine(binary);
+//    Console.WriteLine(binary.Left);
+//    Console.WriteLine(binary.Right);
+//    Console.WriteLine(binary.NodeType);
+//}
+
+
+//var adultEmployees = employees.Where(x => filter.Compile()(x)).ToList();
+//adultEmployees.ForEach(x => Console.WriteLine(x));
+
+
+List<string> clear = new List<string>() { "lorem?", "ipsum{", "dolor.", "sit,", "jbs^" };
+clear.ForEach(text => Console.WriteLine(text));
+
+Expression<Func<string, string>> clearExpression = e => e.ToLower().Replace("?", "").Replace("{", "").Replace(".", "").Replace(",", "").Replace("^", "");
+
+var cleartext = clearExpression.Compile();
+
+var filteredTexts = clear.Select(cleartext).ToList();
+
+filteredTexts.ForEach(text => Console.WriteLine(text));
+
+#endregion
+#region Fourth Example
+
+#endregion
+#region Fifth Example
+
+#endregion
